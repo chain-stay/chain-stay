ba049504140da18653bef089b7697f2d42ea2f0d
arypte <wjs505@gmail.com>
2024-06-02 23:03:14 +0900

    update: interaction

--- frontend/src/hooks/chainStayInteraction.jsx
+++ frontend/src/hooks/chainStayInteraction.jsx
@@ -1,11 +1,11 @@
 import Web3 from 'web3';
-import { chainStayHubAddress } from '../config/Polygon';
+import { chainStayHubAddress } from '../config/polygonAddr';
 import { chainStayABI } from '../config/chainStayABI';
-const web3 = new Web3(window.ethereum);
 
+const web3 = new Web3(window.ethereum);
 const chainStayContract = new web3.eth.Contract(chainStayHubAddress, chainStayABI);
 
-export const onClickExample = async (account, vault_c) => {
+export const onClickExample = async (account) => {
     try {
       await chainStayContract.methods
         .exampleFunction(parameter)
